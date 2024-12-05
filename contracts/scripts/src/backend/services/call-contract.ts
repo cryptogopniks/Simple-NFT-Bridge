9c2e0659e498afe658f8eb5c68a91910d788ee87
M. Daeva <embedmad@gmail.com>
2024-12-05 12:40:42 +0300

    removed unused scripts

--- contracts/scripts/src/backend/services/call-contract.ts
+++ contracts/scripts/src/backend/services/call-contract.ts
@@ -22,7 +22,6 @@ import {
   getCwExecHelpers,
   getCwQueryHelpers,
 } from "../../common/account/cw-helpers";
-import { getFloorPrices } from "./sg-api";
 
 async function main() {
   try {
@@ -43,14 +42,16 @@ async function main() {
       },
     } = getChainOptionById(CHAIN_CONFIG, chainId);
 
-    const LENDING_PLATFORM_CONTRACT = getContractByLabel(
+    const TRANSCEIVER_HUB_CONTRACT = getContractByLabel(
       CONTRACTS,
-      "lending_platform"
+      "transceiver-hub"
     );
 
-    const ORACLE_CONTRACT = getContractByLabel(CONTRACTS, "oracle");
+    const TRANSCEIVER_OUTPOST_CONTRACT = getContractByLabel(
+      CONTRACTS,
+      "transceiver-outpost"
+    );
 
-    // const ORACLE_CONTRACT = getContractByLabel(CONTRACTS, "oracle");
     const gasPrice = `${GAS_PRICE_AMOUNT}${DENOM}`;
     const testWallets = await getWallets(TYPE);
     const { signer, owner } = await getSigner(PREFIX, testWallets.SEED_ADMIN);
@@ -58,8 +59,7 @@ async function main() {
     const sgQueryHelpers = await getSgQueryHelpers(RPC);
     const sgExecHelpers = await getSgExecHelpers(RPC, owner, signer);
 
-    const { scheduler, lending, minter, oracle, utils, marketMaker } =
-      await getCwQueryHelpers(chainId, RPC);
+    const { utils, transceiver } = await getCwQueryHelpers(chainId, RPC);
     const h = await getCwExecHelpers(chainId, RPC, owner, signer);
 
     const { getBalance, getAllBalances, getTimeInNanos } = sgQueryHelpers;
@@ -73,97 +73,6 @@ async function main() {
       COLLECTION.TESTNET?.BAD_KIDS?.NEUTRON || "",
       COLLECTION.TESTNET?.SLOTH?.NEUTRON || "",
     ];
-
-    // // add liquidity to maker maker
-    // for (const [collection] of collectionList) {
-    //   const floorPrice =
-    //     floorPrices.find((x) => x.collection === collection)?.price || 0;
-
-    //   await h.marketMaker.cwDepositLiquidity(
-    //     collection,
-    //     floorPrice * 1e6 * 50,
-    //     paymentDenom,
-    //     gasPrice
-    //   );
-    // }
-
-    // await h.minter.cwMintMultiple(
-    //   TOKEN.NEUTRON.TESTNET.USDC,
-    //   [[owner, 400_000_000_004]],
-    //   gasPrice
-    // );
-    // await minter.cwQueryBalances(owner, true);
-
-    // await h.marketMaker.cwDepositLiquidity(
-    //   COLLECTION.TESTNET?.PIGEON?.NEUTRON || "",
-    //   9 * 20 * 1e6,
-    //   TOKEN.NEUTRON.TESTNET.USDC,
-    //   gasPrice
-    // );
-    // await h.marketMaker.cwDepositLiquidity(
-    //   COLLECTION.TESTNET?.BAD_KIDS?.NEUTRON || "",
-    //   1_700 * 20 * 1e6,
-    //   TOKEN.NEUTRON.TESTNET.USDC,
-    //   gasPrice
-    // );
-    // await h.marketMaker.cwDepositLiquidity(
-    //   COLLECTION.TESTNET?.SLOTH?.NEUTRON || "",
-    //   6_800 * 20 * 1e6,
-    //   TOKEN.NEUTRON.TESTNET.USDC,
-    //   gasPrice
-    // );
-
-    // await oracle.pQueryPrices(9, 9, true);
-
-    const collections = (await marketMaker.pQueryCollectionOwnerList(9)).map(
-      (x) => x.collection_address
-    );
-    const liqudity = await marketMaker.pQueryLiquidityList(9);
-    const prices = await marketMaker.pQueryOfferPricesList(9);
-
-    li({ collections });
-    li({ liqudity });
-    li({ prices });
-
-    return;
-
-    await scheduler.cwQueryConfig(true);
-    await lending.cwQueryAddressConfig(true);
-    await lending.cwQueryCommonConfig(true);
-    await lending.cwQueryRateConfig(true);
-    await oracle.cwQueryConfig(true);
-    await marketMaker.cwQueryConfig(true);
-    return;
-
-    // for (const collectionAddress of [
-    //   COLLECTION.TESTNET?.PIGEON?.NEUTRON || "",
-    //   COLLECTION.TESTNET?.BAD_KIDS?.NEUTRON || "",
-    //   COLLECTION.TESTNET?.SLOTH?.NEUTRON || "",
-    // ]) {
-    //   await h.utils.cwMintNft(
-    //     collectionAddress,
-    //     ADDRESS.TESTNET.NEUTRON.WORKER,
-    //     [4, 5, 6],
-    //     gasPrice
-    //   );
-    // }
-
-    return;
-
-    // const res = await utils.cwV2QueryAsksByCollectionDenom(
-    //   // "stars1fvhcnyddukcqfnt7nlwv3thm5we22lyxyxylr9h77cvgkcn43xfsvgv0pl" ||
-    //   ADDRESS.MAINNET.STARGAZE.MARKETPLACE,
-    //   COLLECTION.MAINNET.STARGAZE.EXPEDITION,
-    //   TOKEN.MAINNET.STARS,
-    //   { descending: false, limit: 5 }
-    // );
-
-    const res = await getFloorPrices([
-      COLLECTION?.MAINNET?.EXPEDITION?.STARGAZE || "",
-      COLLECTION?.MAINNET?.MAD_SCIENTISTS?.STARGAZE || "",
-    ]);
-
-    li(res);
   } catch (error) {
     l(error);
   }
--- contracts/scripts/src/common/config/index.ts
+++ contracts/scripts/src/common/config/index.ts
@@ -7,17 +7,17 @@ export type NetworkName = "STARGAZE" | "NEUTRON";
 
 export type Wasm = "nft_minter.wasm" | "transceiver.wasm";
 
-export type Label = "nft_minter" | "transceiver-hub" | "transceiver-stargaze";
+export type Label = "nft_minter" | "transceiver-hub" | "transceiver-outpost";
 
 export const ADDRESS = {
   MAINNET: {
+    NEUTRON: {
+      ADMIN: "neutron1f37v0rdvrred27tlqqcpkrqpzfv6ddr2dxqan2",
+      WORKER: "neutron1hvp3q00ypzrurd46h7c7c3hu86tx9uf8qt2q28",
+    },
     STARGAZE: {
-      // GOPNIKS_DAO: "stars1u2t3uwgv9rfnfhqya7mmq7xev8nesdy9ae9d5jctlp2hyzwu5rds024g69",
-      // Liquidity DAO
-      ADMIN: "stars1emkds0juacpjvrepkw0jlj503hcswy0msu3mpg0dud8nnq0wqp9s0jzz7a",
-      // SUBDAO
-      WORKER:
-        "stars17rwa9fe7grauqdy3fka5yh0lakr5yffpkrmpymy3yakkl6xy9r2s7ccwgk",
+      ADMIN: "stars1f37v0rdvrred27tlqqcpkrqpzfv6ddr2a97zzu",
+      WORKER: "stars1hvp3q00ypzrurd46h7c7c3hu86tx9uf8sg5lm3",
     },
   },
   TESTNET: {
@@ -78,7 +78,7 @@ export const CHAIN_CONFIG: ChainConfig = {
               LABEL: "nft_minter",
               PERMISSION: [ADDRESS.TESTNET.NEUTRON.ADMIN],
               INIT_MSG: toJson<NftMinterTypes.InstantiateMsg>({
-                cw721_code_id: 8345, // TODO: mainnet 2554,
+                cw721_code_id: 8345,
                 transceiver_hub: $(
                   "OPTIONS[CHAIN_ID=pion-1]|CONTRACTS[LABEL=transceiver-hub]|ADDRESS"
                 ),
@@ -109,6 +109,96 @@ export const CHAIN_CONFIG: ChainConfig = {
                 },
               }),
               CODE: 8431,
+              ADDRESS: "",
+            },
+          ],
+          IBC: [],
+        },
+
+        // TODO: NEUTRON main
+        {
+          TYPE: "main",
+          DENOM: "untrn",
+          CHAIN_ID: "neutron-1",
+          RPC_LIST: ["https://rpc-neutron.cosmos-spaces.cloud:443"],
+          GAS_PRICE_AMOUNT: 0.99,
+          STORE_CODE_GAS_MULTIPLIER: 20,
+          CONTRACTS: [
+            {
+              WASM: "nft_minter.wasm",
+              LABEL: "nft_minter",
+              PERMISSION: [ADDRESS.MAINNET.NEUTRON.ADMIN],
+              INIT_MSG: toJson<NftMinterTypes.InstantiateMsg>({
+                cw721_code_id: 2554,
+                transceiver_hub: $(
+                  "OPTIONS[CHAIN_ID=neutron-1]|CONTRACTS[LABEL=transceiver-hub]|ADDRESS"
+                ),
+              }),
+              MIGRATE_MSG: toJson<NftMinterTypes.MigrateMsg>({
+                version: "1.0.0",
+              }),
+              UPDATE_MSG: toJson({}),
+              CODE: 0,
+              ADDRESS: "",
+            },
+
+            {
+              WASM: "transceiver.wasm",
+              LABEL: "transceiver-hub",
+              PERMISSION: [ADDRESS.MAINNET.NEUTRON.ADMIN],
+              INIT_MSG: toJson<TransceiverTypes.InstantiateMsg>({
+                transceiver_type: "hub",
+              }),
+              MIGRATE_MSG: toJson<TransceiverTypes.MigrateMsg>({
+                version: "1.0.0",
+              }),
+              UPDATE_MSG: toJson<TransceiverTypes.ExecuteMsg>({
+                update_config: {
+                  nft_minter: $(
+                    "OPTIONS[CHAIN_ID=neutron-1]|CONTRACTS[LABEL=nft-minter]|ADDRESS"
+                  ),
+                },
+              }),
+              CODE: 0,
+              ADDRESS: "",
+            },
+          ],
+          IBC: [],
+        },
+      ],
+    },
+
+    {
+      NAME: "stargaze",
+      PREFIX: "stars",
+      OPTIONS: [
+        // TODO: STARGAZE main
+        {
+          TYPE: "main",
+          DENOM: "ustars",
+          CHAIN_ID: "stargaze-1",
+          RPC_LIST: ["https://rpc.stargaze-apis.com:443"],
+          GAS_PRICE_AMOUNT: 1.1,
+          STORE_CODE_GAS_MULTIPLIER: 21.5,
+          CONTRACTS: [
+            {
+              WASM: "transceiver.wasm",
+              LABEL: "transceiver-outpost",
+              PERMISSION: [ADDRESS.MAINNET.STARGAZE.ADMIN],
+              INIT_MSG: toJson<TransceiverTypes.InstantiateMsg>({
+                transceiver_type: "outpost",
+              }),
+              MIGRATE_MSG: toJson<TransceiverTypes.MigrateMsg>({
+                version: "1.0.0",
+              }),
+              UPDATE_MSG: toJson<TransceiverTypes.ExecuteMsg>({
+                update_config: {
+                  hub_address: $(
+                    "OPTIONS[CHAIN_ID=neutron-1]|CONTRACTS[LABEL=transceiver-hub]|ADDRESS"
+                  ),
+                },
+              }),
+              CODE: 0,
               ADDRESS: "",
             },
           ],
